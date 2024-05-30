24c0216e513deca3f281c13101767e4624de5b73
Kyne <kyne.huang@aliyun.com>
2024-05-30 16:34:10 +0800

    update

--- src/usb_camera_udev.sh
+++ src/usb_camera_udev.sh
@@ -1,4 +1,4 @@
-echo  'KERNEL=="video*", ATTRS{idVendor}=="1bcf", ATTRS{idProduct}=="2284", ATTR{index}=="0", MODE:="0777", OWNER:="root", GROUP:="video", SYMLINK+="aoni_a50"' >>/etc/udev/rules.d/camera_1.rules
+echo  'KERNEL=="video*", ATTRS{idVendor}=="1bcf", ATTRS{idProduct}=="2284", ATTR{index}=="0", MODE:="0777", OWNER:="root", GROUP:="video", SYMLINK+="aoni_a50"' >/etc/udev/rules.d/camera_1.rules
 
 service udev reload
 sleep 2
